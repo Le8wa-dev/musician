0bd8371f6f229d1c2dc81cb8596c2921e2714901
Le8wa-dev <savinchev@list.ru>
2019-10-09 18:50:49 +0300

    added section history

--- app/js/main.js
+++ app/js/main.js
@@ -6,5 +6,25 @@ $(function(){
         dots: true,
     });
 
+    $('.history__inner').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        slidesToShow: 3,
+        variableWidth: true,
+    });
+
+    $(window).scroll(function(){
+        if ($(this).scrollTop() > 500) {
+        $('.scrollup').fadeIn();
+        } else {
+        $('.scrollup').fadeOut();
+        }
+    }); 
+        
+    $("a[href^='#']").click(function(){
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
 });
