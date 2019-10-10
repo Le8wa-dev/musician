2d5312caa6b698cd1bbe453c28c67019e3486e4d
Le8wa-dev <savinchev@list.ru>
2019-10-10 19:52:09 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -4,6 +4,21 @@ $(function(){
         prevArrow: '<button class="slick-prev slick-arrow" ><img class="prev" src="images/icons/prevarrow.png" alt=""></button>',
         nextArrow: '<button class="slick-next slick-arrow" ><img class="next" src="images/icons/nextarrow.png" alt=""></button>',
         dots: true,
+        responsive: [
+            {
+                breakpoint: 970,
+                settings: {
+                  arrows: false,
+                }
+              },
+              {
+                  breakpoint: 768,
+                  settings: {
+                    arrows: false,
+                    dots: false,
+                  }
+                },
+        ]
     });
 
     $('.history__inner').slick({
@@ -11,6 +26,18 @@ $(function(){
         centerPadding: '60px',
         slidesToShow: 3,
         variableWidth: true,
+        responsive: [
+            {
+                breakpoint: 850,
+                settings: {
+                    slidesToShow: 1,
+                }
+              },
+        ]
+    });
+
+    $('.header__inner-btn').on('click',function(){
+        $('.header__inner > ul').slideToggle();
     });
 
     $(window).scroll(function(){
