107442f6bbafbe3856fc67bc933b921b49966e5a
Le8wa-dev <savinchev@list.ru>
2019-10-14 16:19:15 +0300

    changed gitignore

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,6 @@
 $(function(){
 
+    // Слайдер SLider
     $('.slider__items').slick({
         prevArrow: '<button class="slick-prev slick-arrow" ><img class="prev" src="images/icons/prevarrow.png" alt=""></button>',
         nextArrow: '<button class="slick-next slick-arrow" ><img class="next" src="images/icons/nextarrow.png" alt=""></button>',
@@ -21,6 +22,7 @@ $(function(){
         ]
     });
 
+    // Слайдер History
     $('.history__inner').slick({
         centerMode: true,
         centerPadding: '60px',
@@ -36,10 +38,12 @@ $(function(){
         ]
     });
 
+    // Всплывающее меню
     $('.header__inner-btn').on('click',function(){
         $('.header__inner > ul').slideToggle();
     });
 
+    // Плавный скролл к якорю
     $(window).scroll(function(){
         if ($(this).scrollTop() > 500) {
         $('.scrollup').fadeIn();
@@ -47,7 +51,8 @@ $(function(){
         $('.scrollup').fadeOut();
         }
     }); 
-        
+    
+    // Кнопка возврата к хедеру
     $("a[href^='#']").click(function(){
         var _href = $(this).attr("href");
         $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
--- gulpfile.js
+++ gulpfile.js
@@ -23,7 +23,7 @@ gulp.task('style', function(){
         'node_modules/normalize.css/normalize.css',
         'node_modules/slick-carousel/slick/slick.css',
         'node_modules/slick-carousel/slick/slick-theme.css',
-        'node_modules/magnific-popup/dist/magnific-popup.css'
+        'node_modules/magnific-popup/dist/magnific-popup.css',
     ])
         .pipe(concat('libs.css'))
         .pipe(gulp.dest('app/css'))
@@ -32,7 +32,7 @@ gulp.task('style', function(){
 gulp.task('script', function(){
     return gulp.src([
         'node_modules/slick-carousel/slick/slick.js',
-        'node_modules/magnific-popup/dist/jquery.magnific-popup.js'
+        'node_modules/magnific-popup/dist/jquery.magnific-popup.js',
     ])
         .pipe(concat('libs.min.js'))
         .pipe(uglify())
